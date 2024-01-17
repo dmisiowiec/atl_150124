c1fa7730e9b7465b380b54a32e23bfd6e18eb180
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-01-17 10:22:28 +0100

    24. Rest API - example CURL

--- rental-store/src/main/java/com/pivovarit/MoviesRestController.java
+++ rental-store/src/main/java/com/pivovarit/MoviesRestController.java
@@ -7,10 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -22,9 +25,12 @@ public class MoviesRestController {
         this.movieService = movieFacade;
     }
 
+    // curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "title": "The Matrix", "type": "NEW"}' http://localhost:8081/movies
     @PostMapping("/movies")
-    void addMovie(MovieAddRequest movie) {
+    Map<String, Long> addMovie(@RequestBody MovieAddRequest movie) {
+        System.out.println("Adding movie: " + movie);
         movieService.save(movie);
+        return Map.of("id", movie.id());
     }
 
     @GetMapping("/movies?type={type}")
