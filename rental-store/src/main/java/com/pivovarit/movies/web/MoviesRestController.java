c158b5f1073178fa21845542d55595d9e490cada
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-01-17 11:49:58 +0100

    26. OpenAPI Specification

--- rental-store/src/main/java/com/pivovarit/movies/web/MoviesRestController.java
+++ rental-store/src/main/java/com/pivovarit/movies/web/MoviesRestController.java
@@ -2,6 +2,8 @@ package com.pivovarit.movies.web;
 
 import com.pivovarit.movies.MovieFacade;
 import com.pivovarit.movies.api.MovieAddRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +25,7 @@ public class MoviesRestController {
     }
 
     // curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "title": "The Matrix", "type": "NEW"}' http://localhost:8081/movies
+    @Operation(description = "Add movie")
     @PostMapping("/movies")
     Map<String, Long> addMovie(@RequestBody MovieAddRequest movie) {
         System.out.println("Adding movie: " + movie);
@@ -30,11 +33,13 @@ public class MoviesRestController {
         return Map.of("id", movie.id());
     }
 
+    @Operation(parameters = @Parameter(name = "type", required = false), description = "Get all movies")
     @GetMapping("/movies")
     Collection<MovieDtoRepresentationModel> getMovies() {
         return movieService.findAll().stream().map(MovieDtoRepresentationModel::from).toList();
     }
 
+    @Operation(hidden = true)
     @GetMapping(value = "/movies", params = "type")
     Collection<MovieDtoRepresentationModel> getMoviesByType(@RequestParam String type) {
         if (type != null) {
@@ -44,23 +49,27 @@ public class MoviesRestController {
         }
     }
 
+    @Operation(description = "Get movie by id")
     @GetMapping("/movies/{id}")
     MovieDtoRepresentationModel getMovieById(@PathVariable int id) {
         return MovieDtoRepresentationModel.from(movieService.findById(id));
     }
 
+    @Operation(description = "Rent movie by id")
     @GetMapping("/movies/{id}/rent")
     ResponseEntity<Void> rentMovie(@PathVariable int id) {
         System.out.println("Renting movie with id: " + id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(description = "Return movie by id")
     @GetMapping("/movies/{id}/return")
     ResponseEntity<Void> returnMovie(@PathVariable int id) {
         System.out.println("Returning movie with id: " + id);
         return ResponseEntity.ok().build();
     }
 
+    @Operation(description = "Find movie by title")
     @GetMapping("/movies/title/{title}")
     public ResponseEntity<MovieDtoRepresentationModel> findByTitle(@PathVariable String title) {
         return movieService.findByTitle(title)
