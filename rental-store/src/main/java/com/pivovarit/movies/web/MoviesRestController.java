2f61b7c2ae82b0014d837a4de9b6fd2ba9bcc6be
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-01-17 12:08:10 +0100

    27. Enable CORS

--- rental-store/src/main/java/com/pivovarit/movies/web/MoviesRestController.java
+++ rental-store/src/main/java/com/pivovarit/movies/web/MoviesRestController.java
@@ -5,6 +5,7 @@ import com.pivovarit.movies.api.MovieAddRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.Map;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class MoviesRestController {
 
